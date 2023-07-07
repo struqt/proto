153f5a4e0a9cab651fe3b809616118cb41b1980d
wkang <wkang2015@163.com>
2023-07-07 10:20:10 +0800

    Fixes

--- download.sh
+++ download.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 set -eo pipefail
 
-declare -rx BUF_DIR='.'
+declare -r SELF=$(readlink -f "$0")
+declare -r SELF_DIR=${SELF%/*}
+declare -r BUF_DIR="${SELF_DIR}"
+declare -r GRPC_GATEWAY_VERSION="${1:-v2.16.0}"
 
-download_protobuf_with_curl() {
+download_file_with_curl() {
   local url="https://raw.githubusercontent.com/${1:?}"
   local path="${BUF_DIR:?}/${2:?}"
-  if [ -f "${path:?}" ]; then
-    echo "File exists: $path"
-    return
-  fi
   mkdir -p "$(dirname "${path:?}")"
-  curl "${url:?}" -o "${path}"
+  curl "${url:?}" -o "${path}" -s
+  echo -e "File downloaded: $url\n ---> $path"
 }
 
-download_protobuf_google_api() {
+download_proto_google_api() {
   local version="${1}"
   download() {
     local file="${1}"
     local repo='googleapis'
     local base_url="googleapis/${repo}/${version:?}/google/api"
     local base_path="${repo}/google/api"
-    download_protobuf_with_curl "${base_url}/${file}" "${base_path}/${file:?}"
+    download_file_with_curl "${base_url}/${file}" "${base_path}/${file:?}"
   }
   download annotations.proto
   download field_behavior.proto
@@ -29,7 +29,7 @@ download_protobuf_google_api() {
   download httpbody.proto
 }
 
-download_protobuf_grpc_gateway() {
+download_proto_grpc_gateway() {
   local version="${1}"
   download() {
     local file="${1}"
@@ -37,12 +37,12 @@ download_protobuf_grpc_gateway() {
     local repo='grpc-gateway'
     local base_url="grpc-ecosystem/${repo}/${version:?}/${package}"
     local base_path="${repo}/${package}"
-    download_protobuf_with_curl "${base_url}/${file}" "${base_path}/${file:?}"
+    download_file_with_curl "${base_url}/${file}" "${base_path}/${file:?}"
   }
   download annotations.proto
   download openapiv2.proto
 }
 
-download_protobuf_google_api master
-download_protobuf_grpc_gateway v2.16.0
+download_proto_google_api master
+download_proto_grpc_gateway "${GRPC_GATEWAY_VERSION:?}"
 echo 'Downloading is finished'
